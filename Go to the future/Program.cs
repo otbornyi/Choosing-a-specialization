dad19a0976fd875d3ed7fb908521ee8dae3342fc
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-27 20:15:09 +0300

    add Fill and Print array methods

---
+++ Go to the future/Program.cs
@@ -0,0 +1,16 @@
+﻿void FillArray(string[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.WriteLine("Введите что нибудь: ");
+        A[i] = Console.ReadLine();
+    }
+}
+void PrintArray(string[] A)
+{
+    for (int i = 0; i < A.Length; i++)
+    {
+        Console.Write($"{A[i]} ");
+    }
+    Console.WriteLine();
+}
