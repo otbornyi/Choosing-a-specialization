bb30e3abb0d35654f783a02556142ae094af0e56
otbornyi <94071821+otbornyi@users.noreply.github.com>
2022-01-27 20:18:18 +0300

    Delete PrintArray as unnecessary

--- Go to the future/Program.cs
+++ Go to the future/Program.cs
@@ -2,15 +2,23 @@
 {
     for (int i = 0; i < A.Length; i++)
     {
-        Console.WriteLine("Введите что нибудь: ");
+        Console.WriteLine("Введите предложение: ");
         A[i] = Console.ReadLine();
     }
 }
-void PrintArray(string[] A)
+
+void Choise(string[] A)
 {
+    string B = String.Empty;
     for (int i = 0; i < A.Length; i++)
     {
-        Console.Write($"{A[i]} ");
+        string Z = A[i];
+        char[] a = Z.ToCharArray();
+        if(a.Length < 4)
+        {
+            B = B + A[i] + " ";
+        }
+
     }
-    Console.WriteLine();
+    Console.WriteLine(B);
 }
